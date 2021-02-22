269678d39df7d34c84fec509b964d9e87a2bbe88
Martin Raedlinger <mr@formatlos.de>
2021-02-22 12:00:19 +0100

    fix: load secrets from ssm

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
-# Hasura - Reload remote schema 
+# AWS System Manager - Get multiple variables 
 
 ![Github Action](https://flat.badgen.net/badge/Github/Action/green?icon=github)
 ![MIT license](https://flat.badgen.net/badge/License/MIT/green)
 [![code style: prettier](https://img.shields.io/badge/code_style-prettier-ff69b4.svg?style=flat-square)](https://github.com/prettier/prettier)
 
-This actions gets multiple parameters from AWS System Manager
+This actions gets multiple parameters from AWS System Manager and maps them to github action secrets
 
 ## Inputs
 
@@ -17,7 +17,7 @@ This actions gets multiple parameters from AWS System Manager
 ```yaml
 uses: ProcureAi/aws-ssm-secrets-multiple@v1.0.0
 with:
-  SSM_PARAMETERS:  |
+  SSM_PARAMETERS: |
     FOO=/app/production/foo,
     BAR=/app/production/bar,
 ```
--- src/index.js
+++ src/index.js
@@ -1,58 +1,48 @@
 const core = require('@actions/core');
 const SSM = require('aws-sdk/clients/ssm');
 
-
 (async () => {
   const ssmParams = core.getInput('SSM_PARAMETERS', { required: true });
 
   core.startGroup('Injecting secret environment variables');
 
-  console.log(ssmParams);
+  const ssm = new SSM();
 
-  const regex = /(\w+)=\s*([^,\s*]*)/gi;
-  let match;
+  let result;
+  try {
+    const regex = /(\w+)=\s*([^,\s*]*)/gi;
+    let match;
+    const params = [];
 
-  while ((match = regex.exec(ssmParams)) !== null) {
-    console.log('FOUND', match[1], match[2]);
-    // expected output: "Found foo. Next starts at 9."
-    // expected output: "Found foo. Next starts at 19."
+    while ((match = regex.exec(ssmParams)) !== null) {
+      params.push({ envKey: match[1].toUpperCase(), ssmParam: match[2] });
+    }
+
+    for (const { envKey, ssmParam } of params) {
+      result = await ssm
+        .getParameter({
+          Name: ssmParam,
+          WithDecryption: true,
+        })
+        .promise();
+
+      const secret = result?.Parameter?.Value;
+
+      if (!secret) {
+        core.warning(`Secret ${envKey} seems to be empty`);
+      }
+
+      console.log('SECRET', secret);
+
+      core.setSecret(secret || '');
+
+      core.exportVariable(envKey, secret);
+      core.info(`Secret ${envKey} injected`);
+    }
+  } catch (error) /* istanbul ignore next */ {
+    core.setFailed(error.message);
+    throw error;
   }
 
-
-  // const ssm = new SSM();
-
-  // let result;
-  // try {
-  //   result = await ssm
-  //     .getParameter({
-  //       Name: actionParam.ssmParameter,
-  //       WithDecryption: true, // NOTE: this flag is ignored for String and StringList parameter types
-  //     })
-  //     .promise();
-  // } catch (error) /* istanbul ignore next */ {
-  //   core.setFailed(error.message);
-  //   throw error;
-  // }
-  //
-  // const envVar = actionParam.envVariable.toUpperCase();
-  // const secret = result?.Parameter?.Value;
-  //
-  // if (!secret) {
-  //   core.warning(`Secret value for environment variable ${envVar} appears to be empty`);
-  // }
-  //
-  // core.setSecret(secret || '');
-  // core.exportVariable(envVar, secret);
-  // core.info(`Successfully set secret environment variable: ${envVar}`);
-
   core.endGroup();
-
-
-
-
-
 })();
-
-
-
-
